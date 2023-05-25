6a587f00dde8915ed0e8fe74b6042ca1420be6af
tangtanglove <dai_hang_love@126.com>
2023-05-25 11:20:56 +0800

    chore: WIP

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -15,8 +15,7 @@
       </nut-swiper-item>
     </nut-swiper>
   </view>
-  <view class="index">
-    <nut-toast :msg="msg2" v-model:visible="show" :type="type" :cover="cover"/>
+  <view>
     <nut-tabbar bottom="false" safe-area-inset-bottom="true" @tab-switch="tabSwitch">
       <nut-tabbar-item tab-title="首页" :icon="Home"></nut-tabbar-item>
       <nut-tabbar-item tab-title="分类" :icon="Category"> </nut-tabbar-item>
@@ -38,26 +37,15 @@ export default {
   },
   setup() {
     const state = reactive({
-      msg: '欢迎使用 NutUI4.0 开发小程序',
-      msg2: '你成功了～',
       type: 'text',
       show: false,
-      cover: false
     });
-
-    const handleClick = (type, msg, cover = false) => {
-      state.show = true;
-      state.msg2 = msg;
-      state.type = type;
-      state.cover = cover;
-    };
 
     return {
       ...toRefs(state),
-      handleClick,
       Home,
       Category,
-      My 
+      My
     }
   }
 }
