6913c457bb08c8887f4c78219f8f84a0b166aef6
tangtanglove <dai_hang_love@126.com>
2023-05-25 11:32:14 +0800

    chore: WIP

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,23 @@
 <template>
-  <view class="banner">
-    <nut-swiper :init-page="page" :pagination-visible="true" pagination-color="#426543" auto-play="3000">
-      <nut-swiper-item>
-        <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg" alt="" />
-      </nut-swiper-item>
-      <nut-swiper-item>
-        <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg" alt="" />
-      </nut-swiper-item>
-      <nut-swiper-item>
-        <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/welcomenutui.jpg" alt="" />
-      </nut-swiper-item>
-      <nut-swiper-item>
-        <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/fristfabu.jpg" alt="" />
-      </nut-swiper-item>
-    </nut-swiper>
-  </view>
-  <view>
-    <nut-tabbar bottom="false" safe-area-inset-bottom="true" @tab-switch="tabSwitch">
-      <nut-tabbar-item tab-title="首页" :icon="Home"></nut-tabbar-item>
-      <nut-tabbar-item tab-title="分类" :icon="Category"> </nut-tabbar-item>
-      <nut-tabbar-item tab-title="我的" :icon="My"></nut-tabbar-item>
-    </nut-tabbar>
-  </view>
+  <nut-swiper :init-page="page" :pagination-visible="true" pagination-color="#426543" auto-play="3000">
+    <nut-swiper-item>
+      <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg" alt="" />
+    </nut-swiper-item>
+    <nut-swiper-item>
+      <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg" alt="" />
+    </nut-swiper-item>
+    <nut-swiper-item>
+      <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/welcomenutui.jpg" alt="" />
+    </nut-swiper-item>
+    <nut-swiper-item>
+      <img class="swiperItem" src="https://storage.360buyimg.com/jdc-article/fristfabu.jpg" alt="" />
+    </nut-swiper-item>
+  </nut-swiper>
+  <nut-tabbar bottom="false" safe-area-inset-bottom="true" @tab-switch="tabSwitch">
+    <nut-tabbar-item tab-title="首页" :icon="Home"></nut-tabbar-item>
+    <nut-tabbar-item tab-title="分类" :icon="Category"> </nut-tabbar-item>
+    <nut-tabbar-item tab-title="我的" :icon="My"></nut-tabbar-item>
+  </nut-tabbar>
 </template>
 
 <script>
