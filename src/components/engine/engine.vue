7caf0f4027522b1e32197837641bac5744ede673
tangtanglove <dai_hang_love@126.com>
2023-05-26 18:00:34 +0800

    chore: wip

--- src/components/engine/engine.vue
+++ src/components/engine/engine.vue
@@ -3,61 +3,60 @@
 </template>
 
 <script>
-	import Taro from '@tarojs/taro'
-	import Render from '@/components/render/render';
-	import { get } from "@/services/action"
-	
-	export default {
-		name: 'Engine',
-		components: {
-			Render,
+import Taro from '@tarojs/taro'
+import Render from '@/components/render/render';
+import { get } from "@/services/action"
+
+export default {
+	name: 'Engine',
+	components: {
+		Render,
+	},
+	props: {
+		title: {
+			type: String,
+			default: '引擎页'
 		},
-		props: {
-			title: {
-				type: String,
-				default: '引擎页'
-			},
-			api: {
-				type: String,
-				default: null
-			}
-		},
-		data() {
-			return {
-				body:{}
-			}
-		},
-		mounted() {
-			if (!this.api) {
-				Taro.showToast({
-					title:"接口不能为空"
-				})
-			} else {
-				this.getComponents(this.api)
-			}
-		},
-		activated() {
-			if (!this.api) {
-				Taro.showToast({
-					title:"接口不能为空"
-				})
-			} else {
-				this.getComponents(this.api)
-			}
-		},
-		methods: {
-			async getComponents(api) {
-				let result = await get({
-					url:api,
-				})
-				this.body = result;
-				Taro.setNavigationBarTitle({
-					title: result.title
-				});
-			}
+		api: {
+			type: String,
+			default: null
+		}
+	},
+	data() {
+		return {
+			body: {}
+		}
+	},
+	mounted() {
+		if (!this.api) {
+			Taro.showToast({
+				title: "接口不能为空"
+			})
+		} else {
+			this.getComponents(this.api)
+		}
+	},
+	activated() {
+		if (!this.api) {
+			Taro.showToast({
+				title: "接口不能为空"
+			})
+		} else {
+			this.getComponents(this.api)
+		}
+	},
+	methods: {
+		async getComponents(api) {
+			let result = await get({
+				url: api,
+			})
+			this.body = result;
+			Taro.setNavigationBarTitle({
+				title: result.title
+			});
 		}
 	}
+}
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
