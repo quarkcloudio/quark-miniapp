f89f4aff5f7f84f5d1b77297f9de52298ce20866
Hang Dai <60868863+quarkcms@users.noreply.github.com>
2023-05-27 14:52:25 +0800

    Update page.vue

--- src/components/page/page.vue
+++ src/components/page/page.vue
@@ -1,45 +1,45 @@
 <template>
-	<view :style="style">
-		<view v-if="navbar">
-			<render :body="this.navbar" />
-		</view>
-		<view>
-			<render :body="this.content" />
-		</view>
-		<view v-if="tabbar">
-			<render :body="this.tabbar" />
-		</view>
-	</view>
+  <view :style="style">
+    <view v-if="navbar">
+      <render :body="this.navbar" />
+    </view>
+    <view>
+      <render :body="this.content" />
+    </view>
+    <view v-if="tabbar">
+      <render :body="this.tabbar" />
+    </view>
+  </view>
 </template>
 
 <script>
 import Engine from '@/components/render/render.vue';
 
 export default {
-	name: 'Page',
-	props: {
-		navbar: {
-			type: [Object],
-			default: null
-		},
-		tabbar: {
-			type: [Boolean, Object],
-			default: null
-		},
-		content: {
-			type: [String, Number, Object],
-			default: ''
-		},
-		style: {
-			type: [String, Object],
-			default() {
-				return {}
-			}
-		}
-	},
-	data() {
-		return {};
-	}
+  name: 'Page',
+  props: {
+    navbar: {
+      type: [Object],
+      default: null
+    },
+    tabbar: {
+      type: [Boolean, Object],
+      default: null
+    },
+    content: {
+      type: [String, Number, Object],
+      default: ''
+    },
+    style: {
+      type: [String, Object],
+      default() {
+          return {}
+      }
+    }
+  },
+  data() {
+    return {};
+  }
 };
 </script>
 
