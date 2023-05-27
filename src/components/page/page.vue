35ebc53815f46134eaee7a7617929e7a1f5c1129
tangtanglove <dai_hang_love@126.com>
2023-05-27 14:39:06 +0800

    chore: wip

--- src/components/page/page.vue
+++ src/components/page/page.vue
@@ -1,47 +1,46 @@
 <template>
 	<view :style="style">
 		<view v-if="navbar">
-      <render :body="this.navbar" />
+			<render :body="this.navbar" />
 		</view>
 		<view>
 			<render :body="this.content" />
 		</view>
-    <view v-if="tabbar">
-      <render :body="this.tabbar" />
+		<view v-if="tabbar">
+			<render :body="this.tabbar" />
 		</view>
 	</view>
 </template>
 
 <script>
-	import Engine from '@/components/render/render.vue';
-	
-	export default {
-		name: 'Page',
-		props: {
-			navbar: {
-				type: [Object],
-				default: null
-			},
-			tabbar: {
-				type: [Boolean,Object],
-				default: null
-			},
-			content: {
-				type: [String, Number, Object],
-				default: ''
-			},
-			style: {
-				type: [String, Object],
-				default () {
-					return {}
-				}
-			}
+import Engine from '@/components/render/render.vue';
+
+export default {
+	name: 'Page',
+	props: {
+		navbar: {
+			type: [Object],
+			default: null
 		},
-		data() {
-			return {};
+		tabbar: {
+			type: [Boolean, Object],
+			default: null
+		},
+		content: {
+			type: [String, Number, Object],
+			default: ''
+		},
+		style: {
+			type: [String, Object],
+			default() {
+				return {}
+			}
 		}
-	};
+	},
+	data() {
+		return {};
+	}
+};
 </script>
 
-<style lang="scss" >
-</style>
+<style lang="scss" ></style>
